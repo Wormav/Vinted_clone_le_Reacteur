cc555e23a109b20ac8f1356ea0250ae12705a43f
Jérémy Lorette <jeremy.lorette@outlook.com>
2023-10-20 11:31:48 +0200

    maj for multiple pictures

--- controllers/offer.controllers.js
+++ controllers/offer.controllers.js
@@ -8,99 +8,100 @@ const {
 const convertToBase64 = require("../utils/convertToBase64");
 
 const publishOffer = async (req, res) => {
-  try {
-    // Get parameters
-    const { title, description, price, condition, city, brand, size, color } =
-      req.body;
+    try {
+        // Get parameters
+        const { title, description, price, condition, city, brand, size, color } = req.body;
 
-    // Check if all parameters are provided
-    if (
-      !title ||
-      !description ||
-      !price ||
-      !condition ||
-      !city ||
-      !brand ||
-      !size ||
-      !color
-    ) {
-      return res.status(400).json({ message: "Missing parameters" });
+        // Check if all parameters are provided
+        if (!title || !description || !price || !condition || !city || !brand || !size || !color) {
+            return res.status(400).json({ message: "Missing parameters" });
+        }
+
+        // Check description length
+        const checkMaxCharDescription = maxCharDescription(description);
+        if (!checkMaxCharDescription)
+            return res.status(400).json({
+                message: "La description ne doit pas dépasser 500 caractères",
+            });
+
+        // Check price
+        const checkMaxPrice = maxPrice(price);
+        if (!checkMaxPrice)
+            return res.status(400).json({
+                message: "Le prix ne doit pas dépasser 100000 euros",
+            });
+
+        // Check title length
+        const maxTitle = maxCharTitle(title);
+        if (!maxTitle)
+            return res.status(400).json({
+                message: "Le titre ne doit pas dépasser 50 caractères",
+            });
+
+        // Create new offer
+        const newOffer = new Offer({
+            product_name: title,
+            product_description: description,
+            product_price: parseInt(price),
+            product_details: [
+                { MARQUE: brand },
+                { TAILLE: size },
+                { ETAT: condition },
+                { COULEUR: color },
+                { EMPLACEMENT: city },
+            ],
+            owner: req.user,
+        });
+
+        // Check if pictures were uploaded
+        let productImagesArray = [];  // This array will store all the Cloudinary results for each image
+        if (req.files && req.files.picture) {
+            const picturesToUpload = Array.isArray(req.files.picture) ? req.files.picture : [req.files.picture];
+
+            for (let i = 0; i < picturesToUpload.length; i++) {
+                const pictureToUpload = picturesToUpload[i];
+                const base64Image = convertToBase64(pictureToUpload);
+                const result = await cloudinary.uploader.upload(base64Image, {
+                    folder: `vinted/offers/${newOffer._id}`,
+                });
+                productImagesArray.push(result);
+            }
+
+            // Set the first image result to product_image
+            newOffer.product_image = productImagesArray[0];
+
+            // If there are more than one image, set the rest to product_pictures
+            if (productImagesArray.length > 1) {
+                newOffer.product_pictures = productImagesArray.slice(1);
+            } else {
+                newOffer.product_pictures = [];
+            }
+        }
+
+        await newOffer.save();
+
+        if (!newOffer) {
+            return res.status(400).json({ message: "Error while saving offer" });
+        }
+
+        // Populate offer with owner
+        const populatedOffer = await Offer.findById(newOffer._id).populate({
+            path: "owner",
+            select: "_id account avatar",
+        });
+
+        // Check if offer was populated
+        if (!populatedOffer) {
+            return res.status(400).json({ message: "Error while retrieving offer" });
+        }
+
+        res.status(200).json({ message: "Offre crée", offer: populatedOffer });
+    } catch (err) {
+        // Send error response
+        res.status(500).json({ message: "Une erreur est survenue", err: err.message });
     }
-
-    // Check description length
-    const checkMaxCharDescription = maxCharDescription(description);
-
-    if (!checkMaxCharDescription)
-      return res.status(400).json({
-        message: "La description ne doit pas dépasser 500 caractères",
-      });
-
-    // Check price
-    const checkMaxPrice = maxPrice(price);
-
-    if (!checkMaxPrice)
-      return res.status(400).json({
-        message: "Le prix ne doit pas dépasser 100000 euros",
-      });
-
-    // Check title length
-    const maxTitle = maxCharTitle(title);
-
-    if (!maxTitle)
-      return res.status(400).json({
-        message: "Le titre ne doit pas dépasser 50 caractères",
-      });
-
-    // Create new offer
-    const newOffer = new Offer({
-      product_name: title,
-      product_description: description,
-      product_price: parseInt(price),
-      product_details: [
-        { MARQUE: brand },
-        { TAILLE: size },
-        { ETAT: condition },
-        { COULEUR: color },
-        { EMPLACEMENT: city },
-      ],
-      owner: req.user,
-    });
-
-    // Check if pictures were uploaded
-    if (req.files && req.files.picture) {
-      const pictureToUpload = req.files.picture;
-      const base64Image = convertToBase64(pictureToUpload);
-      const result = await cloudinary.uploader.upload(base64Image, {
-        folder: `vinted/offers/${newOffer._id}`,
-      });
-      newOffer.product_image = result; // Assuming product_image can store an array of URLs
-    }
-
-    await newOffer.save();
-
-    if (!newOffer) {
-      return res.status(400).json({ message: "Error while saving offer" });
-    }
-
-    // Populate offer with owner
-    const populatedOffer = await Offer.findById(newOffer._id).populate({
-      path: "owner",
-      select: "_id account avatar",
-    });
-
-    // Check if offer was populated
-    if (!populatedOffer) {
-      return res.status(400).json({ message: "Error while retrieving offer" });
-    }
-
-    res.status(200).json({ message: "Offre crée", offer: populatedOffer });
-  } catch (err) {
-    // Send error response
-    res
-      .status(500)
-      .json({ message: "Une erreur est survenue", err: err.message });
-  }
 };
+
 
 const updateOffer = async (req, res) => {
   try {
@@ -110,8 +111,7 @@ const updateOffer = async (req, res) => {
 
     let offer = await Offer.findById(id);
 
-    if (!offer)
-      return res.status(400).json({ message: "Annonce non trouvé !" });
+    if (!offer) return res.status(400).json({ message: "Annonce non trouvé !" });
 
     if (offer.owner._id.toString() !== req.user._id.toString()) {
       return res.status(401).json({
@@ -129,79 +129,94 @@ const updateOffer = async (req, res) => {
       size,
       color,
       deleteImage,
+      deletePictures
     } = req.body;
 
-    // Check description length
     const checkMaxCharDescription = maxCharDescription(description);
-
     if (!checkMaxCharDescription)
       return res.status(400).json({
         message: "La description ne doit pas dépasser 500 caractères",
       });
 
-    // Check price
     const checkMaxPrice = maxPrice(price);
-
     if (!checkMaxPrice)
       return res.status(400).json({
         message: "Le prix ne doit pas dépasser 100000 euros",
       });
 
-    // Check title length
     const maxTitle = maxCharTitle(title);
-
     if (!maxTitle)
       return res.status(400).json({
         message: "Le titre ne doit pas dépasser 50 caractères",
       });
 
-    // Check if user is authorized to update offer
     const updateFields = {
       product_name: title || offer.product_name,
       product_description: description || offer.product_description,
       product_price: price ? parseInt(price) : offer.product_price,
       product_details: {
-        MARQUE: brand || offer.product_details.marque,
-        TAILLE: size || offer.product_details.taille,
-        ETAT: condition || offer.product_details.état,
-        COULEUR: color || offer.product_details.couleur,
-        EMPLACEMENT: city || offer.product_details.emplacement,
+        MARQUE: brand || offer.product_details.MARQUE,
+        TAILLE: size || offer.product_details.TAILLE,
+        ETAT: condition || offer.product_details.ETAT,
+        COULEUR: color || offer.product_details.COULEUR,
+        EMPLACEMENT: city || offer.product_details.EMPLACEMENT,
       },
     };
 
-    // Check if user wants to delete image
+    // Handle main image update
     if (req.files && req.files.picture) {
       if (offer.product_image && offer.product_image.public_id) {
         await cloudinary.uploader.destroy(offer.product_image.public_id);
       }
-      // Upload new image
-      const pictureToUpload = req.files.picture;
+
+      const pictureToUpload = req.files.picture[0]; // First picture
       const base64Image = convertToBase64(pictureToUpload);
       const result = await cloudinary.uploader.upload(base64Image, {
         folder: `vinted/offers/${offer._id}`,
       });
 
       updateFields.product_image = result;
-    } // Check if user wants to delete image
-    else if (deleteImage === "true") {
-      if (offer.product_image && offer.product_image.public_id) {
-        await cloudinary.uploader.destroy(offer.product_image.public_id);
+
+      // Handle other images for product_pictures
+      if (req.files.picture.length > 1) {
+        const additionalPictures = req.files.picture.slice(1);
+        for (let pic of additionalPictures) {
+          const base64Pic = convertToBase64(pic);
+          const additionalResult = await cloudinary.uploader.upload(base64Pic, {
+            folder: `vinted/offers/${offer._id}`,
+          });
+          if (!updateFields.product_pictures) {
+            updateFields.product_pictures = [];
+          }
+          updateFields.product_pictures.push(additionalResult);
+        }
       }
+    }
+
+    // Handle deletion of main image
+    if (deleteImage === "true" && offer.product_image && offer.product_image.public_id) {
+      await cloudinary.uploader.destroy(offer.product_image.public_id);
       updateFields.product_image = null;
     }
 
-    offer = await Offer.findByIdAndUpdate(
-      id,
-      { $set: updateFields },
-      { new: true }
-    );
+    // Handle deletion of specific images from product_pictures
+    if (deletePictures && Array.isArray(deletePictures) && deletePictures.length) {
+      // Destroy images on cloudinary
+      for (let publicId of deletePictures) {
+        await cloudinary.uploader.destroy(publicId);
+      }
+
+      // Filter out the images from the offer's product_pictures array
+      offer.product_pictures = offer.product_pictures.filter(
+        pic => !deletePictures.includes(pic.public_id)
+      );
+    }
+
+    offer = await Offer.findByIdAndUpdate(id, { $set: updateFields }, { new: true });
 
     res.status(200).json({ message: "Annonce modifié", offer });
   } catch (err) {
-    // Send error response
-    res
-      .status(500)
-      .json({ message: "Une erreur est survenue", err: err.message });
+    res.status(500).json({ message: "Une erreur est survenue", err: err.message });
   }
 };
 
